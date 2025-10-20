6fc055eb7cd2837965ffae9b29ea491d8ae30c39
Sabrina <sabreyes01@gmail.com>
2025-10-19 23:41:28 -0400

    contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,10 +5,9 @@ function Contact() {
   return (
     <section id="contact" className="section contact-section">
       <p className="contact-number">04. What's Next?</p>
-      <h2 className="contact-title">Don't Leave Me Hangin'!</h2>
+      <h2 className="contact-title">Don't be a stranger.</h2>
       <p className="contact-description">
-        Although I'm not currently looking for any new opportunities, my inbox is always open. 
-        Whether you have a question or just want to say hi, I'll try my best to get back to you!
+        My inbox is always open. Whether you have a question or just want to say hi, I'll try my best to get back to you!
       </p>
       <a href="mailto:sabreyes01@gmail.com" className="btn-outline btn-large">Say Hello</a>
     </section>
