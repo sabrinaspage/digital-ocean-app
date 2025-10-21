bf0fbf5b6205e3c350a55de84e193bd9d119883a
Sabrina <sabreyes01@gmail.com>
2025-10-20 21:52:45 -0400

    hyperlinks for friend and digital ocean

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,9 @@ function Footer() {
   return (
     <footer className="footer">
       <p className="footer-credit">
-        Built by Sabrina Reyes<br/>
-        Designed by Nicholas Ikenma
+        Built by Sabrina Reyes <br/>
+        Hosted on <a href="https://www.digitalocean.com/" target="_blank" rel="noopener noreferrer" className="highlight">Digital Ocean</a> <br/>
+        Designed by <a href="https://bento.me/unji" target="_blank" rel="noopener noreferrer" className="highlight">Nicholas Ikenma</a>
       </p>
     </footer>
   );
